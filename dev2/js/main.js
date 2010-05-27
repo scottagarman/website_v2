47a4f437c9beb793c2eaf390cd49f1157ce65ae9
Scott A Garman <sag5066@gmail.com>
2010-05-27 19:49:46 -0400

    Added page history mgmt w/ jquery.history.js

--- dev2/js/main.js
+++ dev2/js/main.js
@@ -1,12 +1,25 @@
 $(function (){
-	//Test Link
-	$('#s1').load('home.html', function(a, b, c) {
-	});
+	
+	//HISTORY
+	function load(page) {
+    	//$('#content').load(num +".html");
+    	alert("URLID: " + '#'+page);
+    	$('#'+page).click();
+    }
+	
+    $.history.init(function(url) {
+    	alert("INIT URL: " + url);
+    	if(url == ""){
+    		$('#s1').load('home.html', function(a, b, c) {});
+    	}else{
+    		load(url);
+    	}
+    });
+	
+	//Load Main
+	//$('#s1').load('home.html', function(a, b, c) {});
 
-	//SCROLL SCREEN
-	
-	
-		
+	//SCROLL SCREEN	
 	$('#s2, #s3, #s4, #s5, #s6').css({opacity:0});
 	var selected = $('#s1');
 	//correct version!
@@ -31,6 +44,11 @@ $(function (){
 				});	 			
 			});
 		}
+		//handle history
+		var url = $(this).attr('href');
+        url = url.replace(/^.*#/, '');
+        $.history.load(url);
+        return false;
 	});
 	$('#about').click(function(){
 		if($(selected).attr('id') == 's2'){
@@ -53,6 +71,11 @@ $(function (){
 				});	 			
 			});
 		}
+		//handle history
+		var url = $(this).attr('href');
+        url = url.replace(/^.*#/, '');
+        $.history.load(url);
+        return false;
 	});
 	$('#portfolio').click(function(){
 		if($(selected).attr('id') == 's3'){
@@ -75,6 +98,11 @@ $(function (){
 				});	 			
 			});
 		}
+		//handle history
+		var url = $(this).attr('href');
+        url = url.replace(/^.*#/, '');
+        $.history.load(url);
+        return false;
 	});
 	$('#resume').click(function(){
 		if($(selected).attr('id') == 's4'){
@@ -97,6 +125,11 @@ $(function (){
 				});	 			
 			});
 		}
+		//handle history
+		var url = $(this).attr('href');
+        url = url.replace(/^.*#/, '');
+        $.history.load(url);
+        return false;
 	});
 	$('#blog').click(function(){
 		if($(selected).attr('id') == 's5'){
@@ -119,6 +152,11 @@ $(function (){
 				});	 			
 			});
 		}
+		//handle history
+		var url = $(this).attr('href');
+        url = url.replace(/^.*#/, '');
+        $.history.load(url);
+        return false;
 	});
 	$('#contact').click(function(){
 		if($(selected).attr('id') == 's6'){
@@ -141,132 +179,10 @@ $(function (){
 				});	 			
 			});
 		}
+		//handle history
+		var url = $(this).attr('href');
+        url = url.replace(/^.*#/, '');
+        $.history.load(url);
+        return false;
 	});	
-	
-	/*
-	//SCROLL SCREEN	
-	$('#s2, #s3, #s4, #s5, #s6').css({opacity:0});
-	var selected = $('#s1');
-	//correct version!
-	$('#home').click(function(){
-		if($(selected).attr('id') == 's1'){
-			//nothing
-		}else{
-		$('#s1').show();
-		selected.stop().animate({
-			opacity: 0,
-		  }, 750, function() {
-		    // Animation complete.
-		    selected.hide();
-		});
-		$('#s1').stop().animate({
-			opacity: 1,
-		  }, 750, function() {
-		    // Animation complete.
-		    $('#topText').html("<h1>welcome</h1>");
-		    selected = $(this);
-		});
-		}
-	});
-	$('#about').click(function(){
-		if($(selected).attr('id') == 's2'){
-			//nothing
-		}else{
-		$('#s2').show();
-		selected.stop().animate({
-			opacity: 0,
-		  }, 750, function() {
-		    // Animation complete.
-		    selected.hide();
-		});
-		$('#s2').stop().animate({
-			opacity: 1,
-		  }, 750, function() {
-		    // Animation complete.
-		    $('#topText').html("<h1>about me</h1>");
-		    selected = $(this);
-		});
-		}
-	});
-	$('#portfolio').click(function(){
-		if($(selected).attr('id') == 's3'){
-			//nothing
-		}else{
-		$('#s3').show();
-		selected.stop().animate({
-			opacity: 0,
-		  }, 750, function() {
-		    // Animation complete.
-		    selected.hide();
-		});
-		$('#s3').stop().animate({
-			opacity: 1,
-		  }, 750, function() {
-		    // Animation complete.
-		    $('#topText').html("<h1>portfolio</h1>");
-		    selected = $(this);
-		});
-		}
-	});
-	$('#resume').click(function(){
-		if($(selected).attr('id') == 's4'){
-			//nothing
-		}else{
-		$('#s4').show();
-		selected.stop().animate({
-			opacity: 0,
-		  }, 750, function() {
-		    // Animation complete.
-		    selected.hide();
-		});
-		$('#s4').stop().animate({
-			opacity: 1,
-		  }, 750, function() {
-		    // Animation complete.
-		    $('#topText').html("<h1>resume</h1>");
-		    selected = $(this);
-		});
-		}
-	});
-	$('#blog').click(function(){
-		if($(selected).attr('id') == 's5'){
-			//nothing
-		}else{
-		$('#s5').show();
-		selected.stop().animate({
-			opacity: 0,
-		  }, 750, function() {
-		    // Animation complete.
-		    selected.hide();
-		});
-		$('#s5').stop().animate({
-			opacity: 1,
-		  }, 750, function() {
-		    // Animation complete.
-		    $('#topText').html("<h1>blog</h1>");
-		    selected = $(this);
-		});
-		}
-	});
-	$('#contact').click(function(){
-		if($(selected).attr('id') == 's6'){
-			//nothing
-		}else{
-		$('#s6').show();
-		selected.stop().animate({
-			opacity: 0,
-		  }, 750, function() {
-		    // Animation complete.
-		    selected.hide();
-		});
-		$('#s6').stop().animate({
-			opacity: 1,
-		  }, 750, function() {
-		    // Animation complete.
-		    $('#topText').html("<h1>contact</h1>");
-		    selected = $(this);
-		});
-		}
-	});	
-	*/
 });//end onLoad
